3b071030a1056ca5d4716c3c8681c71ba2153246
ryanhamley <ryan.hamley@mapbox.com>
2019-08-15 13:02:26 -0700

    Basic dev setup

---
+++ src/scripts/index.js
@@ -0,0 +1,10 @@
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicnlhbmhhbWxleSIsImEiOiJjaWszbmluaG8wMDAzdTBrc2Q3Ymk3b3l1In0.BxdMyaYKg_0-LwANjPybNA';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
